ffd2a54f3afd3cdc7e3e0a38858491f3855ffbcc
Aleksandra Sturova <Aleksandra_Sturova@epam.com>
2020-11-08 22:05:13 +0300

    fix: add checks to post request

--- product-service/handlers/createProduct.js
+++ product-service/handlers/createProduct.js
@@ -1,11 +1,12 @@
-import { v4 as uuid } from 'uuid';
+import { v4 } from 'uuid';
 import { getClient } from '../db/db';
 import { RESPONSE } from '../utils/response';
 
 const QUERY_PRODUCTS = 'INSERT INTO products(id, title, description, price) VALUES($1,$2,$3,$4) RETURNING *';
 const QUERY_STOCKS = 'INSERT INTO stocks(product_id, count) VALUES($1,$2) RETURNING *';
+
 export const createProduct = async (event) => {
-  const { title = '', description = '', price, count } = JSON.parse(event.body);
+  const data = JSON.parse(event.body);
   const client = getClient();
 
   try {
@@ -15,11 +16,12 @@ export const createProduct = async (event) => {
 
     await client.connect();
 
-    const id = uuid();
+    const id = v4();
+    const { title = null, description = '', price = 0, count } = data;
     const resultProduct = await client.query(QUERY_PRODUCTS, [id, title, description, price]);
     const resultCount = await client.query(QUERY_STOCKS, [id, count]);
 
-    return RESPONSE._200({ ...resultProduct.rows, ...resultCount.rows });
+    return RESPONSE._200({ ...resultProduct.rows[0], ...resultCount.rows[0] });
   } catch (e) {
     return RESPONSE._500();
   } finally {
