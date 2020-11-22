f7bf1185e38ec656c3e7ed18b8ee172c1d2e2095
Aleksandra Sturova <Aleksandra_Sturova@epam.com>
2020-11-22 15:38:53 +0300

    feat: implement task 6

--- constants/index.js
+++ constants/index.js
@@ -1,8 +1,12 @@
 export const HEADERS = {
   'Content-Type': 'application/json',
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET,OPTIONS,POST,PUT'
+  'Access-Control-Allow-Methods': 'GET,OPTIONS,POST,PUT',
 };
 
 export const S3_BUCKET = 'nodejs-aws-import-product-service';
 export const REGION = 'eu-west-1';
+
+export const API = {
+  PRODUCT_CREATE: 'https://y8cv6h0sql.execute-api.eu-west-1.amazonaws.com/dev/products',
+};
